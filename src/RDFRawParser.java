5048e0e043ec115af444effba13f1ccc0d45accc
Merabi Mamar <mamar.merabi@etu.umontpellier.fr>
2018-10-24 17:43:20 +0200

    parse de requete depuis un fichier text

--- src/RDFRawParser.java
+++ src/RDFRawParser.java
@@ -42,63 +42,71 @@ public final class RDFRawParser {
 
 	public static void main(String args[]) throws IOException {
 
-		long start = System.currentTimeMillis();
-
-
-		Reader reader = new FileReader(
-				"./donnees/100K.rdfxml");
-
-		org.openrdf.rio.RDFParser rdfParser = Rio.createParser(RDFFormat.RDFXML);
-		rdfParser.setRDFHandler(new RDFListener());
-		float elapsedTime = System.currentTimeMillis() - start;
-		elapsedTime = (long) (elapsedTime /  1000F);
-		System.out.println("Rio parse a pris "+ elapsedTime);
-
-
-
-		try {
-			rdfParser.parse(reader, "");
-
-			elapsedTime =  (elapsedTime /  1000F);
-			System.out.println("Rdf.parse"+ elapsedTime);
-
-			dict.makeDictionnary();
-
-            elapsedTime = System.currentTimeMillis() - start;
-            elapsedTime =  (elapsedTime /  1000F);
-			System.out.println("Makedico a pris "+ elapsedTime);
-            //index pos
-            dict.Index_creation(dict.sujet_int,dict.predicat_int,dict.objet_int,dict.spo);
-			dict.Index_creation(dict.predicat_int,dict.objet_int,dict.sujet_int,dict.pos);
-			dict.Index_creation(dict.sujet_int,dict.objet_int,dict.predicat_int,dict.sop);
-			dict.Index_creation(dict.predicat_int,dict.sujet_int,dict.objet_int,dict.pso);
-
-			//index print
-
-			elapsedTime = System.currentTimeMillis() - start;
-			elapsedTime =  (elapsedTime /  1000F);
-			System.out.println("execution terminé en  "+ elapsedTime);
+//		long start = System.currentTimeMillis();
+//
+//
+//		Reader reader = new FileReader(
+//				"./donnees/100K.rdfxml");
+//
+//		org.openrdf.rio.RDFParser rdfParser = Rio.createParser(RDFFormat.RDFXML);
+//		rdfParser.setRDFHandler(new RDFListener());
+//		float elapsedTime = System.currentTimeMillis() - start;
+//		elapsedTime = (long) (elapsedTime /  1000F);
+//		System.out.println("Rio parse a pris "+ elapsedTime);
+//
+//
+//
+//		try {
+//			rdfParser.parse(reader, "");
+//
+//			elapsedTime =  (elapsedTime /  1000F);
+//			System.out.println("Rdf.parse"+ elapsedTime);
+//
+//			dict.makeDictionnary();
+//
+//            elapsedTime = System.currentTimeMillis() - start;
+//            elapsedTime =  (elapsedTime /  1000F);
+//			System.out.println("Makedico a pris "+ elapsedTime);
+//            //index pos
+//            dict.Index_creation(dict.sujet_int,dict.predicat_int,dict.objet_int,dict.spo);
+//			dict.Index_creation(dict.predicat_int,dict.objet_int,dict.sujet_int,dict.pos);
+//			dict.Index_creation(dict.sujet_int,dict.objet_int,dict.predicat_int,dict.sop);
+//			dict.Index_creation(dict.predicat_int,dict.sujet_int,dict.objet_int,dict.pso);
+//
+//			//index print
+//
+//			elapsedTime = System.currentTimeMillis() - start;
+//			elapsedTime =  (elapsedTime /  1000F);
+//			System.out.println("execution terminé en  "+ elapsedTime);
 			Outils outil = new Outils();
 
 			System.out.println("Veuillez entrer une requête");
-			String requete;
+			String requete = " ";
+			String line;
 			BufferedReader br;
 			br = new BufferedReader(new FileReader(new File("./donnees/requetes.txt")));
-			while ((requete = br.readLine()) != null){
-				outil.parse_requete(requete);
+			while ((line = br.readLine()) != null){
+                requete+=" "+ line;
 
+                if(line.contains("}")){
+                    outil.parse_requete(requete);
+                    requete = "";
+                }
+                else if(requete.contains("}")){
+
+                }
 			}
 
-		} catch (Exception e) {
-
-		}
-
-
-
-		try {
-			reader.close();
-		} catch (IOException e) {
-		}
+//		} catch (Exception e) {
+//
+//		}
+//
+//
+//
+//		try {
+//			reader.close();
+//		} catch (IOException e) {
+//		}
 
 
 	}
